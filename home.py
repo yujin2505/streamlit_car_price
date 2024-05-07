56bd5296db443e8ebed273e5460275669853c420
yujin2505 <167831858+yujin2505@users.noreply.github.com>
2024-05-07 10:59:40 +0900

    물결로 변경

--- home.py
+++ home.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 def run_home() :
-    st.subheader('자동차 데이터를 분석하고 예측합니다.')
+    st.subheader('자동차 데이터를 분석하고 예측합니다~!')
     st.text('데이터는 캐글에 있는 Car_Purchasing_Data.csv 파일을 사용했습니다.')
     st.text('탐색적 데이터 분석과 자동차 구매 금액을 예측하는 앱입니다.')
     st.image('./image/car.jpg', use_column_width=True)
